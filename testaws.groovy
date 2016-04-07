eb6ec465bca585b9c2e8137d9a6e3c727fd56504
Elazar Rosenthal <elazar@panix.com>
2016-04-07 14:55:41 -0400

    underdev

--- testaws.groovy
+++ testaws.groovy
@@ -27,7 +27,7 @@ node{
     cmd = x2 +" " +  aws + " ec2 describe-instances"
     echo cmd
     bat cmd
-    cmd2 = x2 + aws + " ec2 run-instances   --image-id ami-3d787d57 --count 1 --instance-type t2.micro --key-name  elazartest1 --security-group-ids sg-27f9af42 --subnet-id subnet-96d526e1  > run3.out"
+    cmd2 = x2 + aws + " ec2 run-instances   --image-id ami-3d787d57 --count 1 --instance-type t2.micro --key-name  elazartest1 --security-group-ids sg-27f9af42 --subnet-id subnet-96d526e1  > run2.out"
    echo cmd2
    bat cmd2
    d= readFile 'run2.out'
