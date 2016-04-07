120a8ded1384648e6383385cd4ab6d55f54ef0d9
Elazar Rosenthal <elazar@panix.com>
2016-04-07 12:37:33 -0400

    underdev

---
+++ testaws.groovy
@@ -0,0 +1,32 @@
+import groovy.json.JsonSlurper
+
+def puts3()
+{
+       
+def jsonSlurper = new JsonSlurper()
+def object = jsonSlurper.parseText('{ "name": "John Doe" }')
+echo object.name
+}
+
+
+
+
+node{
+    stage "1"
+   x =input message: 'enter vars:', parameters: [[$class: 'TextParameterDefinition', defaultValue: '', description: '', name: 'AWSENV']]
+   echo x
+   echo x.inspect()
+    x0 = x.replaceAll("\n", "")
+    x1 = x0.replaceAll(";", "\n")
+    x2 = x1.replaceAll("export", "set")
+    echo "\n\n"
+    echo x2
+    bat x2
+    aws='"c:\\Program Files\\Amazon\\AWSCLI\\aws"'
+    echo aws
+    cmd = x2 +" " +  aws + " ec2 describe-instances"
+    echo cmd
+    bat cmd
+  
+
+}
