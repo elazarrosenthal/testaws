f1e7bb3cb779d80edda6cddc76a2f705b2d63608
Elazar Rosenthal <elazar@panix.com>
2016-04-07 15:00:05 -0400

    underdev

--- testaws.groovy
+++ testaws.groovy
@@ -36,6 +36,10 @@ node{
    def p = jsonSlurper.parseText(d)
    ii = p.Instances[0].InstanceId
    echo ii
+   x2 + aws + " ec2 create-tags --resources  " + ii + "  --tags Key=Name,Value=ElazarTestMAchine " 
+   echo x2
+   cmd x2
+
   
 
 }
