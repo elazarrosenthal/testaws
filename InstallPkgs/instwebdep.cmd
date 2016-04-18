03dcd596f176eb9ecb77fa75902030ab2c5000ff
Elazar Rosenthal <elazar@panix.com>
2016-04-18 13:03:38 -0400

    under dev

--- InstallPkgs/instwebdep.cmd
+++ InstallPkgs/instwebdep.cmd
@@ -1,5 +1,8 @@
 d:
 cd d:\share
-WebPlatformInstaller_x86_en-US.msi /passive /quiet 
-WebPlatformInstaller_amd64_en-US.msi /passive /quiet 
-webpicmd /install /products:wdeploy /accepteula
+rem msiexec /i WebPlatformInstaller_x86_en-US.msi /passive /quiet /l*  logfile1.txt
+rem type logfile1.txt
+msiexec /i WebPlatformInstaller_amd64_en-US.msi /passive /quiet /l* logfile2.txt
+type logfile2.txt
+"C:\Program Files\Microsoft\Web Platform Installer\webpicmd" /install /products:wdeploy /accepteula
+
