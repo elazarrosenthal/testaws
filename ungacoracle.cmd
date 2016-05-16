aa68647f87e423906aa985b7919fca9a2946095e
Elazar Rosenthal <elazar@panix.com>
2016-05-16 13:30:30 -0400

    under dev

---
+++ ungacoracle.cmd
@@ -0,0 +1,5 @@
+unzip -f  ODP.NET_Managed_ODAC12cR4.zip -d ODP.NET_Managed_ODAC12cR4
+d:\share\ODP.NET_Managed_ODAC12cR4\odp.net\managed\x64\configure.bat
+d:\share\ODP.NET_Managed_ODAC12cR4\odp.net\managed\x64\unconfigure.bat
+d:\share\ODP.NET_Managed_ODAC12cR4\odp.net\managed\x86\configure.bat
+d:\share\ODP.NET_Managed_ODAC12cR4\odp.net\managed\x86\unconfigure.bat
